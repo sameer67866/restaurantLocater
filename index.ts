03fb780f87a92acccdfedec74f63178f7009ccc4
sameer67866 <msameer1@student.gsu.edu>
2023-08-07 21:15:44 -0400

    google maps api implementation started

--- index.ts
+++ index.ts
@@ -1,66 +1,153 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-let map: google.maps.Map, infoWindow: google.maps.InfoWindow;
+// // Note: This example requires that you consent to location sharing when
+// // prompted by your browser. If you see the error "The Geolocation service
+// // failed.", it means you probably did not give permission for the browser to
+// // locate you.
+// let map: google.maps.Map, infoWindow: google.maps.InfoWindow;
 
-function initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
-  });
-  infoWindow = new google.maps.InfoWindow();
+// function initMap(): void {
+//   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 6,
+//   });
+//   infoWindow = new google.maps.InfoWindow();
 
-  const locationButton = document.createElement("button");
+//   const locationButton = document.createElement("button");
 
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
+//   locationButton.textContent = "Pan to Current Location";
+//   locationButton.classList.add("custom-map-control-button");
 
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+//   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
 
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position: GeolocationPosition) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
+//   locationButton.addEventListener("click", () => {
+//     // Try HTML5 geolocation.
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(
+//         (position: GeolocationPosition) => {
+//           const pos = {
+//             lat: position.coords.latitude,
+//             lng: position.coords.longitude,
+//           };
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter()!);
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter()!);
-    }
-  });
-}
+//           infoWindow.setPosition(pos);
+//           infoWindow.setContent("Location found.");
+//           infoWindow.open(map);
+//           map.setCenter(pos);
+//         },
+//         () => {
+//           handleLocationError(true, infoWindow, map.getCenter()!);
+//         }
+//       );
+//     } else {
+//       // Browser doesn't support Geolocation
+//       handleLocationError(false, infoWindow, map.getCenter()!);
+//     }
+//   });
+// }
 
-function handleLocationError(
-  browserHasGeolocation: boolean,
-  infoWindow: google.maps.InfoWindow,
-  pos: google.maps.LatLng
-) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
-}
+// function handleLocationError(
+//   browserHasGeolocation: boolean,
+//   infoWindow: google.maps.InfoWindow,
+//   pos: google.maps.LatLng
+// ) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(
+//     browserHasGeolocation
+//       ? "Error: The Geolocation service failed."
+//       : "Error: Your browser doesn't support geolocation."
+//   );
+//   infoWindow.open(map);
+// }
 
-declare global {
-  interface Window {
-    initMap: () => void;
-  }
-}
-window.initMap = initMap;
+// declare global {
+//   interface Window {
+//     initMap: () => void;
+//   }
+// }
+// window.initMap = initMap;
+
+// declare global {
+//   interface Window {
+//       latitude: string;
+//       longitude: string;
+//   }
+// }
+// document.addEventListener("DOMContentLoaded", () => {
+//   const userLat = window.latitude;
+//   const userLog = window.longitude;
+
+// });
+// let map: google.maps.Map;
+// async function initMap(): Promise<void> {
+//   const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+//   map = new Map(document.getElementById("map") as HTMLElement, {
+//     center: { lat: window.latitude, lng: window.longitude },
+//     zoom: 8,
+//   });
+// }
+
+// initMap();
+// TypeScript File: script.ts
+
+
+// declare global {
+//   interface Window {
+//       latitude?: number;
+//       longitude?: number;
+//   }
+// }
+
+// let map: google.maps.Map;
+
+// async function initMap(): Promise<void> {
+//   const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+//   map = new Map(document.getElementById("map") as HTMLElement, {
+//       center: { lat: window.latitude || 0, lng: window.longitude || 0 },
+//       zoom: 8,
+//   });
+// }
+
+
+
+
+// TypeScript File: script.ts
+// declare global {
+//   interface Window {
+//       latitude?: number;
+//       longitude?: number;
+//   }
+// }
+
+// let map: google.maps.Map;
+// let markers: google.maps.Marker[] = [];
+
+// async function initMap(): Promise<void> {
+//   const { Map, places } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+
+//   map = new Map(document.getElementById("map") as HTMLElement, {
+//       center: { lat: window.latitude || 0, lng: window.longitude || 0 },
+//       zoom: 15,
+//   });
+
+//   const service = new places.PlacesService(map);
+
+//   const request = {
+//       location: new google.maps.LatLng(window.latitude, window.longitude),
+//       radius: 40000, // Search radius in meters (adjust as needed)
+//       type: "restaurant",
+//   };
+
+//   service.nearbySearch(request, (results, status) => {
+//       if (status === google.maps.places.PlacesServiceStatus.OK) {
+//           // Loop through each nearby restaurant and add a marker
+//           results.forEach(place => {
+//               const marker = new google.maps.Marker({
+//                   position: place.geometry.location,
+//                   map: map,
+//                   title: place.name,
+//               });
+
+//               markers.push(marker);
+//           });
+//       }
+//   });
+// }
--- socialRestaurant.js
+++ socialRestaurant.js
@@ -1,54 +1,181 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-let map, infoWindow;
+let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
-  });
-  infoWindow = new google.maps.InfoWindow();
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
 
-  const locationButton = document.createElement("button");
-
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        },
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
+  map = new Map(document.getElementById("map"), {
+    center: { lat: latitude, lng: longitude},
+    zoom: 11.56,
   });
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation.",
-  );
-  infoWindow.open(map);
-}
-// window.initMap = initMap;
+ initMap();
+
+
+
+
+// let map;
+// let markers = [];
+// let restaurantData = []; // Store restaurant data here
+
+// async function initMap() {
+//   const { Map, places } = await google.maps.importLibrary("maps");
+
+//   map = new Map(document.getElementById("map"), {
+//     center: { lat: latitude, lng: longitude },
+//     zoom: 11.56,
+//   });
+
+//   const service = new places.PlacesService(map);
+
+//   const request = {
+//     location: new google.maps.LatLng(latitude, longitude),
+//     radius: 10000, // Search radius in meters (adjust as needed)
+//     type: "restaurant",
+//   };
+
+//   service.nearbySearch(request, (results, status) => {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//       // Loop through each nearby restaurant and add a marker with infowindow
+//       results.forEach(place => {
+//         // Filter restaurants based on types (e.g., "restaurant" type)
+//         if (place.types.includes("restaurant")) {
+//           // Customize the marker icon to increase its size
+//           const marker = new google.maps.Marker({
+//             position: place.geometry.location,
+//             map: map,
+//             title: place.name,
+//             icon: {
+//               url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png", // Customize the icon URL
+//               scaledSize: new google.maps.Size(40, 40), // Increase the marker size
+//             },
+//           });
+
+//           markers.push(marker);
+
+//           // Store restaurant data
+//           const restaurantInfo = {
+//             name: place.name,
+//             address: place.vicinity,
+//             rating: place.rating,
+//             // Add other relevant information
+//           };
+
+//           restaurantData.push(restaurantInfo);
+
+//           // Create and attach an infowindow to the marker
+//           const infowindow = new google.maps.InfoWindow({
+//             content: `
+//               <div>
+//                 <h3>${restaurantInfo.name}</h3>
+//                 <p>Address: ${restaurantInfo.address}</p>
+//                 <p>Rating: ${restaurantInfo.rating}</p>
+//                 <!-- Add more information here -->
+//               </div>
+//             `,
+//           });
+
+//           marker.addListener("click", () => {
+//             infowindow.open(map, marker);
+//           });
+//         }
+//       });
+//     }
+//   });
+// }
+
+
+
+//initMap();
+//  console.log(restaurantData)
+//  console.log(markers)
+
+
+
+
+// let map;
+// let markers = [];
+// let restaurantData = []; // Store restaurant data here
+
+// async function initMap() {
+//   // Check if the Google Maps API has been imported and initialized
+//   const { Map, places, LatLng } = await google.maps.importLibrary("maps");
+
+//   // Get the user's current location (latitude and longitude)
+//   if ("geolocation" in navigator) {
+//     navigator.geolocation.getCurrentPosition(
+//       async position => {
+//         const latitude = position.coords.latitude;
+//         const longitude = position.coords.longitude;
+
+//         // Create a new LatLng object
+//         const centerLatLng = new LatLng(latitude, longitude);
+
+//         map = new Map(document.getElementById("map"), {
+//           center: centerLatLng,
+//           zoom: 11.56,
+//         });
+
+//         const service = new places.PlacesService(map);
+
+//         const request = {
+//           location: centerLatLng,
+//           radius: 10000000000,
+//           type: "restaurant",
+//         };
+
+//         service.nearbySearch(request, (results, status) => {
+//           if (status === google.maps.places.PlacesServiceStatus.OK) {
+//             results.forEach(place => {
+//               if (place.types.includes("restaurant")) {
+//                 const marker = new google.maps.Marker({
+//                   position: place.geometry.location,
+//                   map: map,
+//                   title: place.name,
+//                 //   icon: {
+//                 //     url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+//                 //     scaledSize: new google.maps.Size(40, 40),
+//                 //   },
+//                  });
+
+//                 markers.push(marker);
+
+//                 const restaurantInfo = {
+//                   name: place.name,
+//                   address: place.vicinity,
+//                   rating: place.rating,
+//                   // Add other relevant information
+//                 };
+
+//                 restaurantData.push(restaurantInfo);
+
+//                 const infowindow = new google.maps.InfoWindow({
+//                   content: `
+//                     <div>
+//                       <h3>${restaurantInfo.name}</h3>
+//                       <p>Address: ${restaurantInfo.address}</p>
+//                       <p>Rating: ${restaurantInfo.rating}</p>
+//                       <!-- Add more information here -->
+//                     </div>
+//                   `,
+//                 });
+
+//                 marker.addListener("click", () => {
+//                   infowindow.open(map, marker);
+//                 });
+//               }
+//             });
+//           }
+//         });
+//       },
+//       error => {
+//         console.error("Error getting user's location:", error);
+//       }
+//     );
+//   } else {
+//     console.error("Geolocation is not available.");
+//   }
+// }
+
+// // Call the initMap function once the Google Maps API is loaded
+// google.maps.onload = initMap();
